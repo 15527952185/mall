3c32678b1916844d2f86eaf130b2e5ded4ca2e5e
15527952185 <1209231105@qq.com>
2019-07-21 09:55:46 +0800

    aa

--- common/src/main/java/com/myslf/common/CommonConstant.java
+++ common/src/main/java/com/myslf/common/CommonConstant.java
@@ -7,6 +7,7 @@ public class CommonConstant {
 	}
 	public enum TradeEnum implements CommonEnum{
 		SUSSESS("1","成功"),FAIL("2","失败");
+		SUSSESS("1","成功"),FAIL("2","失败");
 		private String code;
 		private String str;
 		TradeEnum(String code,String str){
